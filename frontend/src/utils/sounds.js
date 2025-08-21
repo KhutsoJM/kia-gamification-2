4bac08b31033055ffc10959837c981ace90fe424
KhutsoJM <intern4.kia@gmail.com>
2025-08-21 11:55:32 +0200

    started sound system

---
+++ frontend/src/utils/sounds.js
@@ -0,0 +1,21 @@
+import { Howl } from "howler";
+
+// Sound effects for the FruitFall game
+import click from "./sounds/click-1.wav";
+import click2 from "./sounds/click-2.mp3";
+import bubbleClick from "./sounds/bubble-1.mp3";
+import bubbleClick2 from "./sounds/bubble-2.mp3";
+
+import fruitClick from "./sounds/pop-2.mp3";
+
+const sounds = {
+    click: new Howl({ src: [click], volume: 1 }),
+    click2: new Howl({ src: [click2], volume: 1 }),
+    bubbleClick: new Howl({ src: [bubbleClick], volume: 1 }),
+    bubbleClick2: new Howl({ src: [bubbleClick2], volume: 1.75 }),
+};
+
+export const playSound = (name) => {
+    const sound = sounds[name];
+    if (sound) sound.play();
+};
